40ab606b3818d3bf2d9f243176c69a80415c15c9
JoJo <joehearthstone@gmail.com>
2024-01-09 18:46:55 -0500

    omg it works

--- WebScraper/index.js
+++ WebScraper/index.js
@@ -6,6 +6,23 @@ const express = require('express');
 
 const app = express();
 
+const url = 'https://www.serebii.net/'
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+        $('.post', html).each(function(){
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+        })
+        console.log(articles)
+    }).catch(err => console.log(err))
+
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
-

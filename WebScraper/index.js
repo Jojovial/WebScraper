55a4aed733822814bc0d29d7db879e3947868132
JoJo <joehearthstone@gmail.com>
2024-07-06 16:49:37 -0400

    refactored

--- WebScraper/index.js
+++ WebScraper/index.js
@@ -3,26 +3,34 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
 
-
 const app = express();
+const url = 'https://www.serebii.net/';
 
-const url = 'https://www.serebii.net/'
+const fetchArticles = async () => {
+    try {
+        const response = await axios.get(url);
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const articles = [];
 
-axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
-        $('.post', html).each(function(){
-            const title = $(this).text()
-            const url = $(this).find('a').attr('href')
-            articles.push({
-                title,
-                url
-            })
-        })
-        console.log(articles)
-    }).catch(err => console.log(err))
+        $('.post', html).each(function () {
+            const title = $(this).text().trim(); // Trim to remove unnecessary whitespace
+            const url = $(this).find('a').attr('href');
+            articles.push({ title, url });
+        });
 
+        return articles;
+    } catch (error) {
+        console.error(`Error fetching articles: ${error.message}`);
+        return [];
+    }
+};
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+const logArticles = async () => {
+    const articles = await fetchArticles();
+    console.log(articles);
+};
+
+logArticles();
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
